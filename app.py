b65440a20d17a0b45ad92224ae46054e21c39174
zhaoyi3264 <zhaoyi3264@gmail.com>
2023-08-07 09:39:24 -0700

    Add info about overlapping courses and emoji

--- app.py
+++ app.py
@@ -1,17 +1,25 @@
+import os
+
 import streamlit as st
 
 from util import load_json
 
-tracks = load_json('data.json')
-track_type_course_count = load_json('track_type_course_count.json')
+data_dir = 'data'
 
-st.markdown('# Georgia Tech MSCS Tracks')
+def load_json_in(data_dir, file_name):
+    return load_json(os.path.join(data_dir, file_name))
 
-track = st.selectbox('Track', tracks.keys())
+tracks = load_json_in(data_dir, 'tracks.json')
+track_type_course_count = load_json_in(data_dir, 'track_type_course_count.json')
+track_overlap_courses = load_json_in(data_dir, 'track_overlap_courses.json')
 
-st.metric('Available Courses', track_type_course_count[track]['Total'])
+st.markdown('# :computer: Georgia Tech MSCS Tracks ')
 
-tabs = st.tabs(tracks[track])
+track = st.selectbox(':bulb: Track', tracks.keys())
+
+st.metric(':books: Available Courses', track_type_course_count[track]['Total'])
+
+tabs = st.tabs([f'{e} {t}' for e, t in zip([':lock:', ':thought_balloon:'], tracks[track])])
 for tab, (course_type, course_subset) in zip(tabs, tracks[track].items()):
     with tab:
         # st.metric('Available Courses', track_type_course_count[track][course_type])
@@ -21,4 +29,33 @@ for tab, (course_type, course_subset) in zip(tabs, tracks[track].items()):
             for i, course in enumerate(subset['Courses']):
                 selected += st.checkbox(course, key=f'{course_type}_{course}_{i}')
             if selected > subset['Pick']:
-                st.error('You could do that but it is not necessary.')
+                st.error(':worried: You could do that but it is not necessary.')
+
+st.divider()
+
+other_tracks = set(tracks.keys()) - set([track])
+other_track = st.selectbox(':bulb: Another Track', sorted(other_tracks))
+overlap_courses = track_overlap_courses[track][other_track]
+
+# for other_track, overlap_courses in track_overlap_courses[track].items():
+#     if other_track != track:
+
+overlap_course_count = len(overlap_courses)
+
+if overlap_course_count <= 0:
+    emo = ':cry:'
+elif overlap_course_count <= 3:
+    emo = ':grinning:'
+elif overlap_course_count <= 5:
+    emo = ':smile:'
+else:
+    emo = ':satisfied:'
+
+st.metric(f'{emo} Overlapping Courses', overlap_course_count)
+for overlap_course in overlap_courses:
+    st.markdown(f'- {overlap_course}')
+
+# TODO
+# For each track
+# - Display if online is available, total online available
+# - Display has the most overlap with what other tracks.
--- util.py
+++ util.py
@@ -4,3 +4,20 @@ def load_json(file_name):
     with open(file_name, 'r') as f:
         data = json.load(f)
     return data
+
+def to_json(file_name, obj):
+    with open(file_name, 'w') as f:
+        json.dump(obj, f, indent=4)
+
+def map_dict_value(d, func, *args):
+    return dict(map(lambda k: (k, func(d[k], *args)), d))
+
+def flatten(l):
+    flat = []
+    for e in l:
+        if type(e) == list:
+            for nested_e in flatten(e):
+                flat.append(nested_e)
+        else:
+            flat.append(e)
+    return flat
