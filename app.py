19cdcfac53484be7fb71cb516b6b082e6f977113
zhaoyi3264 <zhaoyi3264@gmail.com>
2023-08-06 21:03:07 -0700

    Add validation and error message for selecting too many courses

--- app.py
+++ app.py
@@ -12,8 +12,10 @@ track = st.selectbox('Track', tracks.keys())
 tabs = st.tabs(tracks[track])
 for tab, (course_type, course_subset) in zip(tabs, tracks[track].items()):
     with tab:
-        # st.markdown(f'## {course_type}')
         for subset in course_subset:
+            selected = 0
             st.markdown(f'_Pick {subset["Pick"]} from:_')
             for i, course in enumerate(subset['Courses']):
-                st.checkbox(course, key=f'{course}_{i}')
+                selected += st.checkbox(course, key=f'{course}_{i}')
+            if selected > subset['Pick']:
+                st.error('You could do that but it is not necessary.')
