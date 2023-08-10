27fc6d86ecd01890221a78f9cacabd43c92788f8
zhaoyi3264 <zhaoyi3264@gmail.com>
2023-08-10 08:35:51 -0700

    Add references page

---
+++ pages/References.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+import util
+
+util.set_title('References')
+
+st.markdown('- [M.S. Computer Science Specializations | College of Computing](https://www.cc.gatech.edu/ms-computer-science-specializations)')
+st.markdown('- [Georgia Tech Computer Science Degrees | Udacity](https://www.udacity.com/georgia-tech)')
+st.markdown('- [Current Courses | OMSCS | Georgia Institute of Technology](https://omscs.gatech.edu/current-courses)')
