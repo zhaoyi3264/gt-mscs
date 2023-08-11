af71c7c95a77aa4155bfeb53aed938adf3858170
zhaoyi3264 <zhaoyi3264@gmail.com>
2023-08-10 22:45:10 -0700

    Show XOR courses

--- Tracks.py
+++ Tracks.py
@@ -35,21 +35,31 @@ for tab, (course_type, course_subset) in zip(tabs, tracks[track].items()):
             selected = {'': 0}
             prev_subarea = None
             subareas = subset.get('Sub-area', [None])
+            xor = subset.get('XOR', {})
+            # Show how many courses to pick
             if subarea_condition := subset.get('Sub-area Condition', ''):
                 subarea_condition = f' ({subarea_condition})'
             st.markdown(f'_Pick {subset["Pick"]} from{subarea_condition}:_')
             for i, (course, subarea) in enumerate(zip_longest(subset['Courses'], subareas)):
+                # Sub-area
+                if subarea != prev_subarea:
+                    st.markdown(f'__{subarea}__')
+                prev_subarea = subarea
+                # Course and free course links
                 left_col, right_col = st.columns([8, 2], gap='small')
                 with left_col:
-                    if subarea != prev_subarea:
-                        st.markdown(f'__{subarea}__')
-                    prev_subarea = subarea
-                    s = st.checkbox(get_omscs_course_link(course), key=f'{course_type}_{course}_{i}')
+                    # Course link
+                    help = ''
+                    if xor_courses := xor.get(course, []):
+                        help = '\n'.join(['Can\'t be taken with:'] + xor_courses)
+                    s = st.checkbox(get_omscs_course_link(course), key=f'{course_type}_{course}_{i}', help=help)
                     if subarea:
                         selected[subarea] = selected.get(subarea, 0) + s
                     selected[''] += s
                 with right_col:
+                    # Free course link
                     st.markdown(get_free_course_link(course), unsafe_allow_html=True)
+            # Validate selected courses
             if selected[''] > subset['Pick']:
                 st.error(':worried: You could do that but it is not necessary.')
             if subset.get('Sub-area Condition'):
